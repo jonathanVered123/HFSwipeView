5e72a06ff929aaa2228c192300cb27ec29fc0797
Jonathan Vered <jonathanv@liveperson.com>
2018-01-24 22:37:17 +0200

    Update to swift version

--- HFSwipeView/Classes/HFSwipeView+AutoSlide.swift
+++ HFSwipeView/Classes/HFSwipeView+AutoSlide.swift
@@ -63,7 +63,7 @@ extension HFSwipeView {
         autoSlideTimer = nil
     }
     
-    public func autoSlideCallback(_ timer: Timer) {
+    @objc public func autoSlideCallback(_ timer: Timer) {
         guard count != 0 else {
             return
         }
@@ -72,3 +72,4 @@ extension HFSwipeView {
         }
     }
 }
+
